2787707d38b73a383d610824012ab5b099585e92
gleatd01 <taylor@buzzedtop.com>
2024-08-28 13:18:36 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -5,9 +5,11 @@ catch {
     iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
 }
 
+scoop install git
+
 scoop bucket add main
 scoop bucket add extras
-scoop install git
+
 scoop install vscode
 scoop install flutter
 scoop install android-clt
