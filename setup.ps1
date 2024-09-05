ce17f2ebe8132a26dd999e449d804aba265f231a
gleatd01 <taylor@buzzedtop.com>
2024-09-05 08:15:58 -0400

    maybe jobs and progress bar

--- setup.ps1
+++ setup.ps1
@@ -11,14 +11,32 @@ scoop bucket add main
 scoop bucket add extras
 scoop bucket add java
 
-scoop install vscode
-scoop install flutter
-scoop install android-clt
-scoop install android-studio
-scoop install oraclejdk-lts@19
-scoop install gh
+Start-Job -ScriptBlock { scoop install vscode } -name code
+Start-Job -ScriptBlock { scoop install flutter } -name flutter
+Start-Job -ScriptBlock { scoop install android-clt } -name aclt
+Start-Job -ScriptBlock { scoop install android-studio } -name astu
+Start-Job -ScriptBlock { scoop install oraclejdk-lts@19 } -name java
+Start-Job -ScriptBlock { scoop install gh } -name gh
+Start-Job -ScriptBlock { scoop install glab } -name gl
 
-code install extension Dart-Code.flutter
+$jobStatus = Get-Job
+While ($jobStatus.State -ne "Completed"){
+     $jobStatus = Get-Job
+     Write-Progress -Activity "Waiting for jobs..." -PercentComplete $x
+     If($x -eq 100){
+          $x = 1
+     }
+    Else{
+         $x += 1
+    }
+
+#Outside the while loop add the following to remove the progress bar when done
+Write-Progress -Activity "Collecting Files..." -Completed
+}
+
+Get-Job | Remove-Job #Unless you need the output of these script then use receive-job first
+
+Start-Job -ScriptBlock { code install extension Dart-Code.flutter }
 
 #Github Auth from token or initiate gh auth login
 try {
