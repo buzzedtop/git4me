56487245157a3519ac03bc41baca196790ad6a08
gleatd01 <taylor@buzzedtop.com>
2024-09-06 12:41:43 -0400

    1.0.6

--- setup.ps1
+++ setup.ps1
@@ -1,28 +1,34 @@
-Write-Output("v1.0.5")
+Write-Output("v1.0.6")
 
 try {
     scoop update
 }
 catch {
-    iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+    #iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+    irm get.scoop.sh | iex
 }
 
-scoop install git
-
-scoop bucket add main
-scoop bucket add extras
-scoop bucket add java
-
-Start-ThreadJob -ScriptBlock { scoop install vscode } -name code
-Start-ThreadJob -ScriptBlock { scoop install flutter } -name flutter
-Start-ThreadJob -ScriptBlock { scoop install android-clt } -name aclt
-Start-ThreadJob -ScriptBlock { scoop install android-studio } -name astu
-Start-ThreadJob -ScriptBlock { scoop install oraclejdk-lts@19 } -name java
-Start-ThreadJob -ScriptBlock { scoop install gh } -name gh
-Start-ThreadJob -ScriptBlock { scoop install glab } -name gl
+Start-Job -ScriptBlock { scoop install git } -name git
 
 Get-Job | Wait-Job
-Start-ThreadJob -ScriptBlock { code install extension Dart-Code.flutter }
+
+Start-Job -ScriptBlock { scoop bucket add main }
+Start-Job -ScriptBlock { scoop bucket add extras }
+Start-Job -ScriptBlock { scoop bucket add java }
+
+Get-Job | Wait-Job
+
+Start-Job -ScriptBlock { scoop install vscode } -name code
+Start-Job -ScriptBlock { scoop install flutter } -name flutter
+Start-Job -ScriptBlock { scoop install android-clt } -name aclt
+Start-Job -ScriptBlock { scoop install android-studio } -name astu
+Start-Job -ScriptBlock { scoop install oraclejdk-lts@19 } -name java
+Start-Job -ScriptBlock { scoop install gh } -name gh
+Start-Job -ScriptBlock { scoop install glab } -name gl
+
+Get-Job | Wait-Job
+
+Start-Job -ScriptBlock { code install extension Dart-Code.flutter }
 
 Wait-Job -Name "gh" {
     #Github Auth from token or initiate gh auth login
