b589857ac42d2b833a661fb4180db7f1d1298483
gleatd01 <taylor@buzzedtop.com>
2024-09-06 11:54:27 -0400

    1.0.2

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,4 @@
-Write-Output("v1.0.1")
+Write-Output("v1.0.2")
 
 try {
     scoop update
@@ -13,24 +13,13 @@ scoop bucket add main
 scoop bucket add extras
 scoop bucket add java
 
-#Test Async Job 1. Iterates through 10 integers and sleeps 1 second in between
-Start-Job –Scriptblock {scoop install vscode}
-Start-Job –Scriptblock {scoop install flutter}
-Start-Job –Scriptblock {scoop install android-clt}
-Start-Job –Scriptblock {scoop install android-studio}
-Start-Job –Scriptblock {scoop install oraclejdk-lts@19}
-Start-Job –Scriptblock {scoop install gh}
-Start-Job –Scriptblock {scoop install glab}
-#Monitor all running jobs in the current sessions until they are complete
-#Call our custom WriteJobProgress function for each job to show progress. Sleep 1 second and check again
-
-#Start-Job -ScriptBlock { scoop install vscode } -name code
-#Start-Job -ScriptBlock { scoop install flutter } -name flutter
-#Start-Job -ScriptBlock { scoop install android-clt } -name aclt
-#Start-Job -ScriptBlock { scoop install android-studio } -name astu
-#Start-Job -ScriptBlock { scoop install oraclejdk-lts@19 } -name java
-#Start-Job -ScriptBlock { scoop install gh } -name gh
-#Start-Job -ScriptBlock { scoop install glab } -name gl
+Start-Job -ScriptBlock { scoop install vscode } -name code
+Start-Job -ScriptBlock { scoop install flutter } -name flutter
+Start-Job -ScriptBlock { scoop install android-clt } -name aclt
+Start-Job -ScriptBlock { scoop install android-studio } -name astu
+Start-Job -ScriptBlock { scoop install oraclejdk-lts@19 } -name java
+Start-Job -ScriptBlock { scoop install gh } -name gh
+Start-Job -ScriptBlock { scoop install glab } -name gl
 
 while((Get-Job | Where-Object {$_.State -ne "Completed"}).Count -gt 0)
 {    
