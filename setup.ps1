97bea2d5c0f25dbf369aad3492686242a0727425
gleatd01 <taylor@buzzedtop.com>
2024-09-27 12:04:55 -0400

    1.0.17 G:\My Drive\ssh\ drive mounting VM Setup and Ubuntu Curl

--- setup.ps1
+++ setup.ps1
@@ -1,10 +1,16 @@
-Write-Output("v1.0.16")
+Write-Output("v1.0.17")
 try {
     scoop update
 }
 catch {
     #iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
     irm get.scoop.sh | iex
+}
+
+if (Test-Path -Path 'G:\My Drive\ssh\') {
+    Write-Host "Drive G is mounted."
+} else {
+    & 'C:\Program Files\Google\Drive File Stream\97.0.1.0\GoogleDriveFS.exe'
 }
 
 Write-Output("Git Install")
@@ -25,9 +31,6 @@ Start-Job -ScriptBlock { scoop install glab } -name gl
 Get-Job | Wait-Job
 Get-Job | Remove-Job
 
-Write-Output("Configuration Start")
-Start-Job -ScriptBlock { code install extension Dart-Code.flutter } -name ext_dart
-
 try {
     Get-Content -path 'G:\My Drive\ssh\mytoken.txt' | gh auth login --with-token 
 } catch {
@@ -39,6 +42,13 @@ Get-Job | Remove-Job
 scoop install 1password-cli
 Invoke-Expression (Get-Content -path 'G:\My Drive\ssh\op.ps1' -Raw)
 
+$userInput = Read-Host "curl ubuntu? y/n"
+if ($userInput -eq "y") {
+    curl http://releases.ubuntu.com/focal/ubuntu-20.04.6-desktop-amd64.iso -UseBasicParsing -b
+} else {
+    Write-Output "Curl Ubuntu Skipped"
+}
+
 $userInput = Read-Host "flutter setup? y/n"
 if ($userInput -eq "y") {
     Write-Output("Flutter Setup Install")
@@ -49,6 +59,19 @@ if ($userInput -eq "y") {
     Start-Job -ScriptBlock { scoop install oraclejdk-lts@19 } -name java
     Get-Job | Wait-Job
     Get-Job | Remove-Job
+    Write-Output("Configuration Start")
+    Start-Job -ScriptBlock { code install extension Dart-Code.flutter } -name ext_dart
 } else {
     Write-Output "Flutter Setup Skipped"
+}
+
+$userInput = Read-Host "VM setup? y/n"
+if ($userInput -eq "y") {
+    Write-Output("qemu  Install")
+    Write-Output("https://saisuman.org/blog/run-ubuntu-in-windows-using-qemu")
+    Start-Job -ScriptBlock { scoop install qemu } -name qemu
+    Get-Job | Wait-Job
+    Get-Job | Remove-Job
+} else {
+    Write-Output "VM Setup Skipped"
 }
