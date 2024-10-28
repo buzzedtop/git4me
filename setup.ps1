a4f3033c4006457b9511b6316d2c20fd573e6d47
gleatd01 <taylor@buzzedtop.com>
2024-10-28 14:52:26 -0400

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -13,8 +13,8 @@ if (!($path = Read-Host "Default path: [$default] (Press Enter), Input new Path"
 if (Test-Path -Path $path) {
     Write-Host "$path is valid."
 } else {
-    if ([System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain().Name -eq 'adws.udayton.edu' & 'C:\Program Files\Google\Drive File Stream\97.0.1.0\GoogleDriveFS.exe'0)) {
-        }
+#    if ([System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain().Name -eq 'adws.udayton.edu' & 'C:\Program Files\Google\Drive File Stream\97.0.1.0\GoogleDriveFS.exe'0)) {
+#        }
 }
 
 Write-Output("Git Install")
