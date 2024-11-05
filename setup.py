99a40756bcdad594444a6b02456eb740f9437d8e
Taylor Gleason <Taylor@buzzedtop.com>
2024-11-04 21:53:55 -0500

    pop

--- setup.py
+++ setup.py
@@ -1,16 +1,33 @@
 import os
 import subprocess
-print("Hello")
-print(os.name)
 platform = os.name;
 
-match platform:
-    case 'nt':
-        print('nt')
-        # Define the PowerShell command you want to execute
-        command = "Get-ChildItem C:\\"
-        # Run the PowerShell command and capture the output
-        result = subprocess.run(["powershell", "-Command", command], capture_output=True, text=True)
-        # Print the output
-        print(result.stdout)
+def ps(cmd):
+    print(cmd)
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    return completed
 
+def term(cmd):
+    print(cmd)
+    p = subprocess.Popen('cmd',
+                     shell=True,
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    return out
+
+def display(message):
+    match platform:
+        case 'nt':
+            ps("Write-Host " + message)
+        case 'posix':
+            term("echo " + message)
+
+if __name__ == "__main__":
+    print("Hello")
+    match platform:
+        case 'nt':
+            display('Windows')
+        case 'posix':
+            display('MacOS')
+            print(term("ls"))
